a95699757667f984cea6464edb41ccd4ce4e8927
pranavnulkar <nulkarpranav99@gmail.com>
2024-10-02 15:45:08 +0530

    Non Generic ArrayList And HashTable

---
+++ Collections/ArrayListNonGenerics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Collections
+{
+    internal class ArrayListNonGenerics
+    {
+        static void Main()
+        {
+            //Object Of array List
+            ArrayList arraylst = new ArrayList();
+            arraylst.Add(101);
+            arraylst.Add(true);
+            arraylst.Add(false);
+            arraylst.Add("A");
+            arraylst.Add("Good Morning");
+            arraylst.Add(10.10D);
+            arraylst.Add(2500L);
+            arraylst.Add(2.99F);
+            arraylst.Add("P");
+            arraylst.Add(25000L);
+
+            Console.WriteLine("ArrayList Element:-");
+            foreach (var list in arraylst)
+            {
+                Console.WriteLine(list);
+            }
+
+            //Arraylist Operations
+            arraylst.Remove(true);
+            arraylst.RemoveAt(5);
+            arraylst.RemoveRange(0, 2);
+            arraylst.Reverse();
+
+            Console.WriteLine("Arraylist After Applying Arraylist Operations");
+            foreach(var list in arraylst)
+            {
+                Console.WriteLine(list);
+            }
+        }
+
+    }
+}
---
+++ Collections/HashTableNonGenerics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Collections
+{
+    internal class HashTableNonGenerics
+    {
+        static void Main()
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add(01, "Audi");
+            hash.Add(02, "BMW");
+            hash.Add(03, "Porshe");
+            hash.Add(04, "Range Rover");
+            hash.Add(05, "Toyota");
+            hash.Add(06, "Chevrolet");
+            hash.Add(07, "Kia");
+            hash.Add(08, "Maruti Suzuki");
+            hash.Add(09, "Ford");
+            hash.Add(10, "Tata");
+
+            Console.WriteLine("Hash Table Element With Key Value Pair");
+            foreach (DictionaryEntry table in hash)
+            {
+                Console.WriteLine($"{table.Key} {table.Value}");
+            }
+
+            hash.Remove(05);
+            hash.Remove(09);
+
+            Console.WriteLine("Hashtable After Applying Operation:- ");
+            foreach (DictionaryEntry table in hash)
+            {
+                Console.WriteLine($"{table.Key} {table.Value}");
+            }
+        }
+    }
+}
