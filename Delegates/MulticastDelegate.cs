157be262d51acadeda4cff0d92c7ba46d2c092e0
pranavnulkar <nulkarpranav99@gmail.com>
2024-10-11 10:15:16 +0530

    Assignment On Delegate Unicast and Multicast

---
+++ Delegates/MulticastDelegate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Assignments.Delegates
+{
+    public delegate void Operation(int a, int b);
+
+    class MathOperations
+    {
+        public void AddNum(int a, int b)
+        {
+            Console.WriteLine($"Addition Of Two Numbers:- {a + b}");
+        }
+
+        public void SubNum(int a, int b)
+        {
+            Console.WriteLine($"Subtraction Of Two Numbers:- {a - b}");
+        }
+
+        public void MulNum(int a, int b)
+        {
+            Console.WriteLine($"Multiplication Of Two Numbers:- {a * b}");
+        }
+    }
+
+    class MulticastDelegate
+    {
+        static void Main()
+        {
+            // MathOperation class object
+            MathOperations mo = new MathOperations();
+
+            //create delegate instance
+            //create delegete chain using += and pass the prameter
+            Operation op = new Operation(mo.AddNum);
+            op += mo.SubNum;
+            op += mo.MulNum;
+
+            op += (int a, int b) => Console.WriteLine($"Division of Two number Is:- {a / b}");
+
+            op(10, 5);
+        }
+
+    }
+}
---
+++ Delegates/UnicastDelegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Assignments.Delegates
+{
+    public delegate int AddDel(int a, int b);
+    public delegate int SubDel(int a, int b);
+
+    class OperationDel
+    {
+        public int AddNum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubNum(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    class UnicastDelegate
+    {
+        static void Main()
+        {
+            OperationDel od = new OperationDel();
+
+            AddDel ad = new AddDel(od.AddNum);
+            int x =ad(50, 70);
+            Console.WriteLine($"Addition Of Two Number Is:- {x}");
+
+            SubDel sd = new SubDel(od.SubNum);
+            int y = sd(200, 120);
+            Console.WriteLine($"Subtraction Of Two Numbers Is:- {y}");
+        }
+    }
+}
