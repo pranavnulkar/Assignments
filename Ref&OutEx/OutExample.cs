89b081b35ce1a01f37852f25ffd2af7262c36708
pranavnulkar <nulkarpranav99@gmail.com>
2024-10-18 09:16:17 +0530

    Assignment On Ref And Out Keyword

---
+++ Ref&OutEx/OutExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Ref_OutEx
+{
+    class OutExample
+    {
+        static void Main()
+        {
+            //Two Way to Declare The Parameters
+            //int Num;
+            //String text;
+
+            //Calling The Function
+            InitializeValue(out int Num, out string text);
+
+            Console.WriteLine($"Number:- {Num}");
+            Console.WriteLine($"String:- {text}");
+        }
+
+        static void InitializeValue(out int number, out string message)
+        {
+            //Assign The Integer Number And String Messge Using Out 
+            number = 101;
+            message = "Welcome To Code-Mind Technology";
+        }
+    }
+}
---
+++ Ref&OutEx/RefExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments.Ref_OutEx
+{       
+    class RefExample
+    {
+        static void IncrementNum(ref int a)
+        {
+            a++;
+        }
+
+        static void Main()
+        {
+            int x = 10;
+            Console.WriteLine($"Before Increment Value of x Is:- {x}");
+            //passing value of x to method using ref
+            IncrementNum(ref x);
+            Console.WriteLine($"After Increment Value Of x Is:- {x}");
+        }
+    }
+}
